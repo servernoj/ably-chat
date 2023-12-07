e5d8fdee93be78cfebab3225ef49ec2cb323c96b
Simon Baev <simon.baev@bsci.com>
2023-12-06 21:35:30 -0700

    working draft without message history

---
+++ src/components/message.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string
+  timestamp: string
+  text: string
+}>()
+</script>
+
+<template>
+  <div class="message-container">
+    <img src="@/assets/avatar-svgrepo-com.svg" class="image">
+    <article class="content">
+      <section class="header">
+        <h3 class="name">
+          {{ props.name }}
+        </h3>
+        <span class="timestamp">{{ props.timestamp }}</span>
+      </section>
+      <p class="text">
+        {{ props.text }}
+      </p>
+    </article>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .message-container {
+    width: 100%;
+    padding: 0.5em;
+    display: flex;
+    align-items: flex-start;
+    .image {
+      height: 50px;
+    }
+    .content {
+      margin-left: 1em;
+      display: flex;
+      flex-direction: column;
+      display: flex;
+      align-items: flex-start;
+      .header {
+        display: flex;
+        align-items: center;
+        .name {
+          margin: 0
+        }
+        .timestamp {
+          margin-left: 1em;
+          color: grey;
+        }
+      }
+      .text {
+        margin-top: 0.5em;
+      }
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,78 @@
 <script setup lang="ts">
 import { useStorage } from '@vueuse/core'
 import { useRouter } from 'vue-router'
-import { onMounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import type { QuibleTokens } from './Login.vue'
 import { useLoading } from 'vue-loading-overlay'
 import * as Ably from 'ably'
+import axios from 'axios'
 import 'vue-loading-overlay/dist/css/index.css'
+import Message from '@/components/message.vue'
 
+type MessageItem = {
+  name: string,
+  timestamp: string,
+  text: string
+}
+
+type User = {
+  email: string
+  full_name: string,
+  id: string,
+  phone: string,
+  username: string
+}
+// constants
+const EventMessage = 'Message'
+const ChannelName = 'chat:main'
+// reactive state
+const realtime = ref<Ably.Types.RealtimePromise>()
+const channel = ref<Ably.Types.RealtimeChannelPromise>()
+const input = ref('')
+const me = ref<User|null>(null)
+const messages = ref<MessageItem[]>([])
+// composables
 const router = useRouter()
 const quibleTokens = useStorage<QuibleTokens>('tokens', {})
-const realtime = ref<Ably.Types.RealtimePromise>()
-const clientId = ref('')
-const logout = () => {
+// Handlers
+const getUser = (userId: string) => axios<User>({
+  method: 'GET',
+  url: `${import.meta.env.VITE_QUIBLE_API}/user/${userId}`,
+  headers: {
+    Authorization: `Bearer ${quibleTokens.value.access_token}`
+  }
+})
+  .then(({ data }) => data)
+  .catch(error => {
+    console.error(error)
+    return null
+  })
+const onLogout = () => {
   quibleTokens.value = null
   router.push({ name: 'Login' })
 }
+const onSend = async () => {
+  if (channel.value) {
+    await channel.value.publish(EventMessage, input.value)
+  }
+  input.value = ''
+}
+const onChatMessage = async (message: Ably.Types.Message) => {
+  const user = await getUser(message.clientId)
+  if (user) {
+    messages.value.push({
+      name: user.full_name,
+      timestamp: new Date(message.timestamp).toLocaleString(),
+      text: message.data
+    })
+  }
+}
+
+onUnmounted(
+  async () => {
+    await channel.value?.unsubscribe(EventMessage, onChatMessage)
+  }
+)
 
 onMounted(
   async () => {
@@ -28,9 +86,14 @@ onMounted(
       },
       authUrl: `${import.meta.env.VITE_QUIBLE_API}/rt/token`
     })
-    await realtime.value.auth.authorize()
+    await realtime.value.connection.once('connected')
+    // channel
+    channel.value = realtime.value.channels.get(ChannelName)
+    await channel.value.subscribe(EventMessage, onChatMessage)
+    // clientId
+    me.value = await getUser(realtime.value.auth.clientId)
+    // spinner
     loader.hide()
-    clientId.value = realtime.value.auth.clientId
   }
 )
 
@@ -40,13 +103,33 @@ onMounted(
   <main class="root">
     <nav class="navigation">
       <section class="left">
-        <h3>{{ clientId }}</h3>
+        <h3>{{ me?.full_name ?? "" }}</h3>
       </section>
       <section class="right">
-        <a href="" class="link" @click.prevent="logout">Logout</a>
+        <a href="" class="link" @click.prevent="onLogout">Logout</a>
       </section>
     </nav>
-    <article class="content" />
+    <article class="chat">
+      <section class="messages">
+        <Message
+          v-for="msg,idx in messages"
+          :key="idx"
+          :name="msg.name"
+          :timestamp="msg.timestamp"
+          :text="msg.text"
+        />
+      </section>
+      <form class="input">
+        <input
+          v-model="input"
+          type="text"
+          placeholder="enter your message here"
+        >
+        <button type="submit" :disabled="!input.length" @click.prevent="onSend">
+          Send
+        </button>
+      </form>
+    </article>
   </main>
 </template>
 
@@ -70,7 +153,26 @@ onMounted(
       color: blue;
     }
   }
-  .content {
+  .chat {
+    padding: 1em;
     flex: 1 0 0;
+    display: flex;
+    flex-direction: column;
+    .messages {
+      flex: 1 0 0;
+      padding: 1em;
+      overflow-y: scroll;
+    }
+    .input {
+      width: 100%;
+      display: flex;
+      input {
+        flex: 1 0 0;
+      }
+      button {
+        margin-left: 1em;
+        flex-basis: 100px;
+      }
+    }
   }
 </style>
